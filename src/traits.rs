afb531736360b75034dfb8d1019a3499db6d1999
J.A Sory <87502258+JASory@users.noreply.github.com>
2021-10-20 08:08:47 -0700

    Update traits.rs

--- src/traits.rs
+++ src/traits.rs
@@ -1,47 +1,79 @@
-pub trait AddIdentity{
-    fn add_identity()->Self;
+pub trait Set {}
+
+pub trait Magma : Set {
+    fn op(&self,other: Self)->Self;
 }
 
-pub trait AddInverse{
-    fn add_inverse(&self)->Self;
+pub trait Group: Magma {
+    fn op_identity()->Self;
+    fn op_inverse(&self)->Self;
 }
 
-pub trait AddOperation{
-    fn addition(self, other: Self)->Self;
+
+pub trait AddIdentity : Magma {
+    fn add_identity()-> Self;
 }
 
-pub trait MulIdentity{
-    fn mul_identity()->Self ;
+pub trait AddInverse: Magma{
+    fn add_inverse(&self)-> Self;
 }
 
-pub trait MulInverse{
-   fn mul_inverse(&self)->Self;
+pub trait MulIdentity: Magma {
+    fn mul_identity()-> Self;
 }
 
-pub trait MulOperation{
-   fn multiply(self, other: Self)->Self;
+pub trait MulInverse: Magma{
+    fn mul_inverse(&self)-> Self;
 }
 
-pub trait AdditiveGroup: AddIdentity + AddInverse + AddOperation {}
-
-pub trait MultiplicativeGroup: MulIdentity + MulInverse + MulOperation {}
-
-pub trait SemiRing: AddIdentity + MulIdentity + AddOperation + MulOperation {}
-
-pub trait Ring: SemiRing + AddInverse {} 
-
-pub trait Field: AdditiveGroup + MultiplicativeGroup {}
+pub trait Scalar{}
 
 
-pub trait UFD: SemiRing {
-    
+pub trait AdditiveGroup : std::ops::Add<Output = Self> + AddIdentity + AddInverse + Sized {}
+
+pub trait MultiplicativeGroup : std::ops::Mul<Output = Self> + MulIdentity + MulInverse + Sized {}
+
+pub trait SemiRing : std::ops::Add<Output = Self> + std::ops::Mul<Output = Self> + AddIdentity + MulIdentity + Sized {}
+
+pub trait Ring : SemiRing + AddInverse {}
+
+pub trait Integral : Ring {}
+
+pub trait IntegralClosed: Integral {}
+
+pub trait GCD : SemiRing {
+    fn gcd(&self, other: Self)->Self;
+}
+
+pub trait UFD : GCD {
     fn irreducible(self)->bool;
+    fn factor(self)->Vec<Self>;
 }
 
+pub trait PID : UFD {}
 
-
-pub trait EuclideanDomain {
-    fn remainder(self,other: Self)->Self;
+pub trait Euclidean : GCD {   // Quotient
+    fn euclidean(self, other: Self)->(Self,Self);
     fn form(&self, x: Self, c: Self)->bool;
-    fn gcd(self, other: Self)->Self;
+}
+
+pub trait Field: Ring + MulInverse {}
+
+pub trait Module : Ring + Scalar {}
+
+pub trait VectorSpace: Field + Scalar {}
+
+pub trait BiProduct{}
+
+pub trait FieldAlgebra : Field + BiProduct {}
+
+pub trait RingAlgebra : Module + BiProduct {}
+
+
+pub trait PartiallyOrdered : Set {}
+
+pub trait Lattice: PartiallyOrdered {
+    
+    fn infinum(&self)->Self;
+    fn suprenum(&self)->Self;
 }
