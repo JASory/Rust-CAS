0609e9180fb9c50c63d24888e4b98703e5260c2a
J.A Sory <87502258+JASory@users.noreply.github.com>
2021-11-27 22:23:04 -0700

    Update hypercomplex.rs

--- src/hypercomplex.rs
+++ src/hypercomplex.rs
@@ -3,14 +3,15 @@ use std::convert::TryInto;
 
    use std::ops::*;
    use crate::traits::*;
+   use crate::blas1::*;
    
    
   #[derive(Clone,Copy,Debug, PartialEq)]
-  pub  struct HyperComplex<T, const S: usize>{
+  pub  struct HyperComplex<T, const S: usize> where T: Clone + Set{
                re: [T;S],
               }
               
- impl<T: Clone, const S: usize> HyperComplex<T,S>{
+ impl<T: Set + Clone, const S: usize> HyperComplex<T,S>{
  
  pub fn unchecked_new(re: [T;S])->Self{
         HyperComplex{re}
@@ -22,6 +23,7 @@ use std::convert::TryInto;
          }
            return None ;
        }
+       
  pub fn unchecked_val(&self, x: usize)->T{
      self.re[x].clone()
  }
@@ -34,11 +36,30 @@ use std::convert::TryInto;
          let one  :  [T; S] = x.try_into().unwrap_or_else(|_| panic!("vector is wrong length"));
         HyperComplex::<T,S>::unchecked_new(one)
     }
+   
+  pub fn unchecked_from_slice(x: &[T])->Self{  
+          let mut k = vec![];
+          k.extend_from_slice(x);
+          Self::unchecked_from_vec(k)
+  }
  
  }
  
- impl<T, const S: usize> Set for HyperComplex<T,S>{}
-  impl<T: Clone, const S: usize> Magma for HyperComplex<T,S>{
+ impl<T: Set + Clone, const S: usize> Set for HyperComplex<T,S>{
+      fn rand()->Self{
+          let mut k = vec![];
+            for i in 0..S{
+               k.push(T::rand())
+             }
+          Self::unchecked_from_vec(k)
+      }
+      
+      fn format(&self)->String{
+          self.re.iter().map(|x| x.format()).collect::<Vec<String>>().join(",")
+      }
+ }
+ 
+  impl<T: Set + Clone, const S: usize> Magma for HyperComplex<T,S>{
       fn op(&self, other: Self)->Self{other}
   }
  
@@ -59,9 +80,7 @@ use std::convert::TryInto;
  
     fn add_inverse(&self)->Self{
           let mut k = self.clone();
-            for i in k.re.iter_mut(){
-               *i= i.add_inverse();
-            }
+            add_inv(&mut k.re[..]);
         k
        }
  }
@@ -85,13 +104,11 @@ use std::convert::TryInto;
  } 
  
 
- impl<T: Add<Output =T> + Clone, const S: usize >Add for HyperComplex<T,S>{
+ impl<T: Add<Output =T> + Clone + Set, const S: usize >Add for HyperComplex<T,S>{
              type Output = Self;
     fn add(self, other: Self)->Self{
        let mut k = self.clone();
-       for (i,j) in k.re[..].iter_mut().zip(other.re[..].iter()){
-            *i= i.clone() + j.clone();
-        }
+        add(&other.re[..],&mut k.re[..]);
         k
     }
  
@@ -121,10 +138,29 @@ use std::convert::TryInto;
    impl<T: AddInverse + Clone, const S: usize> HyperComplex<T,S>{
  
  pub fn  conj(&self)->Self{
-         let mut k = self.add_inverse();
-           k.re[0]=k.re[0].add_inverse();
+         let mut k = self.clone();
+          add_inv(&mut k.re[1..]);
          k
       }
+ }
+ /*
+     Operations for f64 Hypercomplex extend to all Fields if possible
+ */
+ 
+ 
+ impl<const S: usize> HyperComplex<f64,S>{
+ 
+ pub fn norm(&self)->f64{
+         self.norm_sqr().sqrt()
+ }
+ 
+ pub fn normalize(&mut self){
+          let norm = self.norm();
+         for i in self.re.iter_mut(){
+            *i /=norm
+         }
+ }
+ 
  }
 /*
     Standard Complex
@@ -134,19 +170,22 @@ use std::convert::TryInto;
  impl<T: Ring + Clone > Mul for HyperComplex<T,2> {
                 type Output = Self;
  fn mul(self, other: Self)->Self{
-      Self::unchecked_new(
-        [self.re[0].clone()*other.re[0].clone() + (self.re[1].clone()*other.re[1].clone()).add_inverse(),
-          self.re[0].clone()*other.re[1].clone() + self.re[1].clone()*other.re[0].clone()]
-     )
+    Self::unchecked_new(
+    [ self.re[0].clone()*other.re[0].clone() + (self.re[1].clone()*other.re[1].clone()).add_inverse(),
+      self.re[0].clone()*other.re[1].clone() + self.re[1].clone()*other.re[0].clone()]
+    )
  }
  
  }
  
- impl<T: Ring + MulInverse + Div<Output = T> + Clone> MulInverse for HyperComplex<T,2>{
+ 
+ 
+ impl<T:  Field + Clone> MulInverse for HyperComplex<T,2>{
  
     fn mul_inverse(&self)->Self{
+        let norm_inv = self.norm_sqr().mul_inverse();
        Self::unchecked_new(
-       [self.re[0].clone() / self.norm_sqr() , (self.re[1].clone()/self.norm_sqr()).add_inverse() ]
+       [self.re[0].clone() * norm_inv.clone() , self.re[1].clone().add_inverse()* norm_inv ]
        )
     }
  }
@@ -157,15 +196,7 @@ use std::convert::TryInto;
  pub  fn arg(&self)->f64{
       self.re[1].atan2(self.re[0])
    }
-   
- pub  fn norm(&self)->f64{
-       self.norm_sqr().sqrt()
-   }
-      // ratio of self / norm
- pub  fn normalize(&self)->Self{
-       Self::unchecked_new([self.re[0]/self.norm(),self.re[1]/self.norm()])
- }  
- 
+  
  pub  fn to_polar(&self)->(f64,f64){
        ( self.norm(), self.arg() )
    }
@@ -206,11 +237,11 @@ use std::convert::TryInto;
     Traits up to Field for Standard Complex numbers
  */
  
- impl<T: AdditiveGroup + Clone> AdditiveGroup for HyperComplex<T,2> {} 
- impl<T: AdditiveGroup + MultiplicativeGroup + Div<Output =T> + Ring + Clone> MultiplicativeGroup for HyperComplex<T,2>{}
- impl<T: AdditiveGroup + MultiplicativeGroup + Div<Output =T> + Ring   + Clone> SemiRing for HyperComplex<T,2>{}
- impl<T: AdditiveGroup + MultiplicativeGroup + Div<Output =T> + Ring  + Clone> Ring for  HyperComplex<T,2>{}
- impl<T: Field + AdditiveGroup + MultiplicativeGroup + Div<Output =T> + Ring + Clone> Field for HyperComplex<T,2>{}
+ impl<T:  AdditiveGroup + Clone> AdditiveGroup for HyperComplex<T,2> {} 
+ impl<T:  Field + Clone> MultiplicativeGroup for HyperComplex<T,2>{}
+ impl<T:  Field + Clone> SemiRing for HyperComplex<T,2>{}
+ impl<T:  Field + Clone> Ring for  HyperComplex<T,2>{fn characteristic()->u64 {0u64}}
+ impl<T:  Field + Clone> Field for HyperComplex<T,2>{}
  
  
  impl   HyperComplex<u64,2>{
@@ -239,9 +270,16 @@ use std::convert::TryInto;
             return false
          }
      }
- 
+ /*    
+ pub  fn factor(self)->Vec<Self>{
+      
+         
+    
+ }    
+ */
  }
  
+
  
  /*
  
@@ -251,43 +289,44 @@ use std::convert::TryInto;
  */
  
  
+ /*
+    Quaternions 
+ 
+ */
+ 
  
  
  //Quaternion operations
- impl<T:  Mul<Output=T> + Add<Output =T> + Sub<Output =T> + Clone > Mul for HyperComplex<T,4> {
+ impl<T:  Ring + Clone > Mul for HyperComplex<T,4> {
          type Output = Self;
   fn  mul(self, other: Self)->Self{
       Self::unchecked_new([
-      self.re[0].clone()*other.re[0].clone()  - self.re[1].clone()*other.re[1].clone() - 
-      self.re[2].clone()*other.re[2].clone() - self.re[3].clone()*other.re[3].clone(),
-         
-      self.re[0].clone()* other.re[1].clone() + self.re[1].clone()*other.re[0].clone() + 
-      self.re[2].clone()*other.re[3].clone() - self.re[3].clone()*other.re[2].clone(),
-         
-      self.re[0].clone()* other.re[2].clone() - self.re[1].clone()*other.re[3].clone() + 
+      self.re[0].clone()*other.re[0].clone() + self.re[1].clone()*other.re[1].clone().add_inverse() + 
+      self.re[2].clone()*other.re[2].clone().add_inverse() + self.re[3].clone()*other.re[3].clone().add_inverse(),
+      
+      self.re[0].clone()*other.re[1].clone() + self.re[1].clone()*other.re[0].clone() + 
+      self.re[2].clone()*other.re[3].clone() + self.re[3].clone()*other.re[2].clone().add_inverse(),
+      
+      self.re[0].clone()*other.re[2].clone() + self.re[1].clone()*other.re[3].clone().add_inverse() + 
       self.re[2].clone()*other.re[0].clone() + self.re[3].clone()*other.re[1].clone(),
-         
-      self.re[0].clone()* other.re[3].clone() + self.re[1].clone()*other.re[2].clone() - 
-      self.re[2].clone()*other.re[1].clone() + self.re[3].clone()*other.re[0].clone()
+      
+      self.re[0].clone()*other.re[3].clone() + self.re[1].clone()*other.re[2].clone() + 
+      self.re[2].clone()*other.re[1].clone().add_inverse() + self.re[3].clone()*other.re[0].clone()
       ])
   }
    
  }
  
+ 
  impl HyperComplex<f64,4>{
  
-  pub  fn norm(&self)->f64{
-          self.norm_sqr().sqrt()
+  pub  fn mul_inverse(&self)-> Self{
+       let n = self.norm_sqr();
+       Self::unchecked_new([
+       self.re[0]/n, self.re[1].add_inverse()/n,self.re[2].add_inverse()/n, self.re[3].add_inverse()/n
+       ])
   }
  
-  pub  fn normalize(&self)->Self{
-          Self::unchecked_new([self.re[0]/self.norm(),self.re[1]/self.norm(),self.re[2]/self.norm(),self.re[3]/self.norm()] )
-  }
-  /*
-  pub mul_inverse(&self)-> Option<Self>{
-       
-  }
- */
  }
  
   //Lipschitz
@@ -297,7 +336,99 @@ use std::convert::TryInto;
          self.norm_sqr().irreducible()
      } 
  }
+ 
+ /*  Octonions */
+ 
+ impl<T:  Ring + Clone + Copy  + std::fmt::Debug> Mul for HyperComplex<T,8> {
+         type Output = Self;
+  fn  mul(self, other: Self)->Self{
+        let (a,b) = ( HyperComplex::<T,4>::unchecked_new([self.re[0], self.re[1], self.re[2], self.re[3]]), 
+                      HyperComplex::<T,4>::unchecked_new([self.re[4], self.re[5], self.re[6], self.re[7]]) );
+        let (c,d) = ( HyperComplex::<T,4>::unchecked_new([other.re[0], other.re[1], other.re[2], other.re[3]]), 
+                      HyperComplex::<T,4>::unchecked_new([other.re[4], other.re[5], other.re[6], other.re[7]]) );
+                      
+        let part1 = (a.clone() * c.clone()) +   (d.conj().clone() * b.clone() ).add_inverse() ;
+               // println!("{:?}",&part1.re[..]);
+        let part2 = ( d * a ) +   (b * c.conj())      ;
+        HyperComplex::<T,8>::unchecked_new([part1.re[0],part1.re[1],part1.re[2],part1.re[3], part2.re[0], part2.re[1], part2.re[2], part2.re[3]])
+ }
+ }
+ 
+ impl HyperComplex<f64,8>{
+ 
   
+  
+  pub fn mul_inverse(&self)->Self{
+                 let n = self.norm_sqr();
+         Self::unchecked_new([
+       self.re[0]/n, self.re[1].add_inverse()/n,self.re[2].add_inverse()/n, self.re[3].add_inverse()/n,
+       self.re[4]/n, self.re[5].add_inverse()/n,self.re[6].add_inverse()/n, self.re[7].add_inverse()/n,
+       ])
+  }  
+      
+ }
+ 
+  /*  Gaussian, Eisenstein, Kleinian  (-1,-3,-7)
+   pub struct QuadraticInteger<const D: i64>{
+               re : [i64;2]
+   }
+       
+       impl QuadraticInteger<const D: i64>{
+            
+            fn unchecked_new(re: [i64;2])->Self{
+                Self{re}
+            }
+            
+           // fn new
+            
+            fn add_identity()->Self{
+                Self::unchecked_new([0i64, 0i64])
+            }
+            
+            fn add_inverse(&self){
+                 Self::unchecked_new([-self.re[0], -self.re[1]])
+            }
+            
+            fn add(&self, other: Self)->Self{
+                Self::unchecked_new(self.re[0]+other.re[0], self.re[1]+other.re[1])
+            }
+            
+            fn mul(&self, other: Self)->Self{
+                if D < 0i64 {
+                   if 4i64-(D%4i64) == 1{// Check if -D mod 4 == 1
+                      return  Self::unchecked_new([
+                      
+                      ])
+                   }
+                   else {// 
+                     return Self::unchecked_new([
+                       self.re[0]*other.re[0] + self.re[1]*other.re[1]*D,
+                        self.re[0]*other.re[1] + self.re[1]*other.re[0]
+                        ])
+                   }
+                }
+                else{
+                   if (D%4i64) == 1{// Check if D mod 4 == 1
+                      
+                   }
+                   else {
+                   return Self::unchecked_new([
+                        self.re[0]*other.re[0] + self.re[1]*other.re[1]*D,
+                        self.re[0]*other.re[1] + self.re[1]*other.re[0]
+                        ])
+                   }
+                } 
+            }
+            
+            fn norm(&self)->i64{
+                 ((self.re[0]*self.re[0] - D*self.re[1]*self.re[1]) as f64).sqrt() as i64
+            }
+            
+            fn conj(&self)->Self{
+                Self::unchecked_new(self.re[0], -self.re[1])
+            }
+       }
+  */
   
     #[derive(Clone, Copy,Debug)]
   pub  struct Dual<T>{
@@ -319,7 +450,8 @@ use std::convert::TryInto;
     }  
   }
   
-  impl<T: Add<Output = T> + Mul<Output = T> + Clone> Mul for Dual<T>{
+  impl<T: Add<Output = T> + Mul<Output = T> 
++ Clone> Mul for Dual<T>{
          type Output = Self;
     fn mul(self, other: Self)->Self{
        Self::unchecked_new(self.a.clone()*other.a.clone(), self.a*other.b + self.b*other.a)
