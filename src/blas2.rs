5b73f03a0f37742e0f47961647d50c0126af4386
J.A Sory <87502258+JASory@users.noreply.github.com>
2021-11-27 22:19:15 -0700

    Update blas2.rs

--- src/blas2.rs
+++ src/blas2.rs
@@ -1,20 +1,43 @@
 /*
 
-  BLAS Level 2
+  BLAS Level 2 matrix-vector operations
 
 */
 
    use std::ops::*;
    use crate::traits::*;
+   use crate::blas1::*;
    use crate::matrix::Matrix;
 
 // Matrix vector operations
-impl<T> Matrix<T>{
+impl<T: SemiRing + PartialEq + Clone > Matrix<T>{
 
          // matrix vector multiply
-  pub fn gemv
+  pub fn unchecked_gemv(&self, x: &Vec<T>)->Self{
+           let (m,n) = (self.row_num(),self.col_num());
+           let mut interim = Matrix::<T>::unchecked_new(1,m,vec![]);
+
+          for i in 0..m{
+             interim.elements.push(dot(&self.elements[(n*i)..(n*(i+1))],&x[..]))
+          }
+          interim
+  } 
+  
+  pub fn unchecked_gaxpy(&self,x: &Vec<T>, y: &Vec<T>)->Self{
+           let (m,n) = (self.row_num(),self.col_num());
+           let mut interim = Matrix::<T>::unchecked_new(1,m,vec![]);
+
+          for i in 0..m{
+             interim.elements.push(dot(&self.elements[(n*i)..(n*(i+1))],&x[..]) + y[i].clone())
+          }
+          interim
+  }/*
+  
+  pub fn outer_product
          // rank 1 operation
   pub fn ger
          // rank 2 operation (symmetric)?
-  pub fn syr
+  pub fn syr */
 }
+
+
